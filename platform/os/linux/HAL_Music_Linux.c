d39f7833f7cab484ff9ef4503c8b8e2f4e9235c7
hubertxxu-mac <hubertxxu@tencent.com>
2025-07-07 18:17:16 +0800

    fix : modify

--- platform/os/linux/HAL_Music_Linux.c
+++ platform/os/linux/HAL_Music_Linux.c
@@ -8,7 +8,7 @@
  * @copyright
  *
  * Tencent is pleased to support the open source community by making IoT Hub available.
- * Copyright(C) 2018 - 2024 THL A29 Limited, a Tencent company.All rights reserved.
+ * Copyright(C) 2018 - 2024 Tencent.All rights reserved.
  *
  * Licensed under the MIT License(the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
